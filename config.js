09a35c698643b3cb5061a46481aa038002f2709c
Arseny Smoogly <arseny@smoogly.ru>
2023-03-23 17:27:57 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,24 @@
+const targets = {
+    grafana: /^@grafana\/ui(?!.*\.s?css$)/,
+    ant: /^(antd|@ant-design)/,
+    mui: /^(@mui|@material-ui)/,
+    chakra: /^chakra-ui/,
+    headlessui: /^@headlessui/,
+    bootstrap: /^react-bootstrap/,
+    visx: /^@visx/,
+    spectrum: /^@adobe\/react-spectrum/,
+    "iot-app-kit": /^@iot-app-kit/,
+    nivo: /^@nivo/,
+    primereact: /^primereact/,
+    rc: /^rc-/, // https://react-component.github.io/
+    reactstrap: /^reactstrap/,
+};
+
+export default [
+  {
+    repoUrl: "https://github.com/percona/grafana-dashboards.git",
+    subprojectPath: "/pmm-app",
+    isTargetModuleOrPath: targets,
+    maxWeeks: 20,
+  },
+]
